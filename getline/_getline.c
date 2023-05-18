69520b8a464e43f1c3e05959e9be2e73b03c467b
Jonathan Boller <5533@holbertonstudents.com>
2023-05-18 09:11:37 -0500

    asdf

---
+++ getline/_getline.c
@@ -0,0 +1,6 @@
+#include "_getline.h"
+
+char *_getline()
+{
+    
+}
---
+++ getline/_getline.h
@@ -0,0 +1,9 @@
+#ifndef _GETLINE_H
+#define _GETLINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+#endif
