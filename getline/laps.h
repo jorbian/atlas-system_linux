be95a4d8e2cdd854260afa6465312f99b9a22add
jorbian <5533@holbertonstudents.com>
2023-05-22 15:53:44 -0500

    kernal style

--- getline/laps.h
+++ getline/laps.h
@@ -19,7 +19,7 @@ typedef struct list_t
 } car_list;
 
 void check_id(car_list **head, size_t id);
-void free_list(car_list *head);
+void free_list(car_list *first_car);
 void print_list(car_list **head);
 car_list *add_nodeint(car_list **head, const int car, int lap);
 void race_state(int *id, size_t size);
