c2926d9146d29c5d5b64716f28fccacfd8eb0d33
Jonathan Boller <5533@holbertonstudents.com>
2023-05-31 14:37:36 -0500

    Fixed betty again

--- ls/main.c
+++ ls/main.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * main - this is the entry point for the program
+ * @argc: num of arguments
+ * @argv: what those arguments were
+ * 
+ * Return: 0
+*/
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	(void)argv; 
+	(void)argv;
 
 	return (0);
 }
