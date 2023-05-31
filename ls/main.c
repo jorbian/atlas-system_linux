b92dab4cc030001b5ff6a8f710707ea6411fa317
Jonathan Boller <5533@holbertonstudents.com>
2023-05-31 14:26:48 -0500

    Fix betty

--- ls/main.c
+++ ls/main.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
-    
-    return (0);
+	(void)argc;
+	(void)argv;
+
+	return (0);
 }
--- ls/my_strings.c
+++ ls/my_strings.c
@@ -3,7 +3,7 @@
 /**
  * _strnlen - returns the length of a string
  *
- * @s: the string we are counting the characters of
+ * @string: the string we are counting the characters of
  * @max: the maximum number of characters to count
  * Return: the length of the string
  */
@@ -24,31 +24,31 @@ int _strnlen(char *string, int max)
 /**
  * _strndup - Returns a pointer to a newly allocated space in memory, which
  * contains a copy of the string given as a parameter.
- * @str: the string to copy
- * @max: the maximum number of characters to copy
+ * @old_string: the string to copy
+ * @max_len: the maximum number of characters to copy
  * Return: the address of the copied string, NULL if failed
  */
 
-char *_strndup(char *str, int maxlen)
+char *_strndup(char *old_string, int max_len)
 {
 	int x = 0;
 	int size = 0;
 	char *new_string;
 
-	if (str == NULL)
+	if (old_string == NULL)
 		return (NULL);
 
-	size  = _strnlen(str, maxlen);
+	size  = _strnlen(old_string, max_len);
 	new_string = malloc(size * sizeof(char) + 1);
 	if (new_string == NULL)
 		return (NULL);
 
 	while (x < size)
 	{
-		*(new_string + x) = *(str + x);
+		*(new_string + x) = *(old_string + x);
 		x++;
 	}
 	*(new_string + x) = '\0';
-    
+
 	return (new_string);
 }
