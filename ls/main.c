d62b5608ff21976a23c01caf2b29b3a2468c4266
jorbian <5533@holbertonstudents.com>
2023-06-03 10:48:17 -0500

    Test to see if this works.

--- ls/main.c
+++ ls/main.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-#include <dirent.h>
-#include <string.h>
+#include <unistd.h>
 
-/**
- * main - entry point for the function
- * Return: returns 0
-*/
-int main(void)
+void execute_system(const char* command)
 {
-	DIR *dir;
-	struct dirent *dp;
-	char *file_name;
+    const char* shell = "/bin/sh";
+    const char* argv[] = {shell, "-c", command, NULL};
+    
+    asm volatile (
+        "mov $0, %%rax\n\t" /* Set the rax register to 0 */
+        "mov $0x3b, %%eax\n\t" /* Set the lower 32 bits of rax to 0x3b (sys_execve) */
+        "mov %0, %%rdi\n\t" /* Set the rdi register to the shell path address */
+        "mov %1, %%rsi\n\t" // Set the rsi register to the argv array address
+        "xor %%rdx, %%rdx\n\t" /* Set rdx register to 0 */
+        "syscall\n\t" /* Call the Linux system call */
+        :
+        : "r"(shell), "r"(argv)
+        : "%rax", "%rdi", "%rsi", "%rdx"
+    );
+}
 
-	dir = opendir(".");
-	while ((dp = readdir(dir)) != NULL)
-	{
-		file_name = dp->d_name;
-		if (!(file_name[0] == '.'))
-			printf("%s\n", file_name);
-	}
-	closedir(dir);
+int main() {
 
-	return (0);
+    execute_system("ls"); // Execute the command (output will be captured by the pipe)
+
+    return (0);
 }
