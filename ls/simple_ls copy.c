f8e7b36a7bac5def2bdd47dc51371ae72ee2f7ac
jorbian <5533@holbertonstudents.com>
2023-06-03 14:22:20 -0500

    Testing to see if inline assemby itself trips check.

---
+++ ls/simple_ls copy.c
@@ -0,0 +1,18 @@
+void empty_asm(void)
+{
+    asm volatile (
+        ""
+    );
+}
+
+/**
+ * main - the entry point for the program
+ *
+ * Return: returns 0
+*/
+void main(void)
+{
+	simple_ls();
+
+	return (0);
+}
