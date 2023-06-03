9b45552579ed8327c9df6e0eb852af79f17f0b7f
jorbian <5533@holbertonstudents.com>
2023-06-03 14:27:12 -0500

    Removed comments.

--- ls/test.c
+++ ls/test.c
@@ -5,12 +5,12 @@ int main(void)
     int result;
 
     asm volatile (
-        "mov $2, %%eax\n\t"  // Move the value 2 into the EAX register
-        "add $2, %%eax\n\t"  // Add 2 to the value in the EAX register
-        "mov %%eax, %0\n\t"  // Move the value in the EAX register to 'result'
-        : "=r"(result)       // Output constraint to bind 'result' to EAX register
-        :                    // No input constraints
-        : "%eax"             // Clobbered register (EAX)
+        "mov $2, %%eax\n\t"
+        "add $2, %%eax\n\t"
+        "mov %%eax, %0\n\t" 
+        : "=r"(result)
+        : 
+        : "%eax"            
     );
 
     printf("The result is: %d\n", result);
