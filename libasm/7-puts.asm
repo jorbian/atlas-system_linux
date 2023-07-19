109edd5020053bf76ea1c9417a1c7f06d1090e7a
jorbian <5533@holbertonstudents.com>
2023-07-19 18:18:10 -0500

    Upload of the last few couple of files.

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -8,13 +8,14 @@ asm_puts:
 	push rdx
 	mov rsi, rdi
 	call asm_strlen		; calculate the length of the string
+
 	mov rdx, rax		; move the string length into rdx
-	mov rdi, 1		; stdout as file descriptor
-	mov rax, 1		; tee up the sys_write syscall
-	syscall			; invoke the syscall
+	mov rdi, 1			; stdout as file descriptor
+	mov rax, 1			; tee up the sys_write syscall
+	syscall				; invoke the syscall
 	mov rax, rdx		; move the contents of rdx into rax
 	pop rdx
 	pop rsi
 	pop rdi
-	ret			; return to the caller
+	ret					; return to caller
 
