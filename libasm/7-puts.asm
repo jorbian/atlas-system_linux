4bf6ee1e4b121c57ceb2cdd8ee26d40cc628578a
jorbian <5533@holbertonstudents.com>
2023-07-19 17:12:42 -0500

    Inital upload of eight task

---
+++ libasm/7-puts.asm
@@ -0,0 +1,20 @@
+BITS 64
+	global asm_puts
+	extern asm_strlen
+
+asm_puts:
+	push rdi
+	push rsi
+	push rdx
+	mov rsi, rdi
+	call asm_strlen		; calculate the length of the string
+	mov rdx, rax		; move the string length into rdx
+	mov rdi, 1		; stdout as file descriptor
+	mov rax, 1		; tee up the sys_write syscall
+	syscall			; invoke the syscall
+	mov rax, rdx		; move the contents of rdx into rax
+	pop rdx
+	pop rsi
+	pop rdi
+	ret			; return to the caller
+
