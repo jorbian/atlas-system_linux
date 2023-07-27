4cd08f6ff25d36cab9f57082b705a6b6f6c66193
jorbian <5533@holbertonstudents.com>
2023-07-26 23:11:55 -0500

    Fixed betty for real this time

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -1,6 +1,12 @@
 #include <sys/types.h>
 #include <signal.h>
 
+/**
+ * pid_exist - does the given PID exist?
+ * @pid: a process id
+ * 
+ * Return: 0 for for no, 1 for yes
+*/
 int pid_exist(pid_t pid)
 {
 	return (kill(pid, 0) == 0);
