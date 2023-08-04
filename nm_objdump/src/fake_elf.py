01c4c4118205fb02d6456b42c83f887abe2908b9
jorbian <5533@holbertonstudents.com>
2023-08-03 21:53:02 -0500

    Finally fixed issue with it freezing on certain files.

--- nm_objdump/src/fake_elf.py
+++ nm_objdump/src/fake_elf.py
@@ -5,21 +5,28 @@ import subprocess
 
 def run_fake_program(filename="a.out", program="hls", command_string="ls"):
     """RUNS RUN OF THE TWO PROGRAMS FOR THS PROJECT I HAVEN'T WRITTEN YET..."""
+    command_args = command_string.format(filename).split(" ")
+
     x = subprocess.Popen(
-        command_string.format(filename).split(" "),
+        command_args,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        bufsize=0,
+        text=True
     )
-    output_message = (x.stderr.read() or x.stdout.read()).decode()
+    output_message, error_message = x.communicate()
+    if x.returncode != 0:
+        output_message = error_message
+
     error_happened = not (output_message.find(program[1:]))
 
-    if (error_happened):
+    if error_happened:
         output_message = ("./{}{}".format(program[0], output_message))
         print_to, exit_stat = sys.stderr, 1
     else:
         print_to, exit_stat = sys.stdout, 0
 
-    print(output_message, file=print_to, end="")
+    print(output_message, file=print_to, end="")  # Decode output_message if using text mode
 
     exit(exit_stat)
 
