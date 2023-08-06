39e88726013c7635ffe71412c496eca84805f030
jorbian <5533@holbertonstudents.com>
2023-08-06 05:29:41 -0500

    Adding the basic files

---
+++ 0x08_CPython/myPy.h
@@ -0,0 +1,10 @@
+#ifndef MYPY_H
+#define MYPY_H
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+void print_python_string(PyObject *p);
+void print_python_int(PyObject *p);
+
+#endif
