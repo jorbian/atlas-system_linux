7bd8b4e2057ec1219e452786e1f24a61224db69d
jorbian <5533@holbertonstudents.com>
2023-09-13 11:03:35 -0500

    Seeing if this works.

--- multithreading/20-tprintf.c
+++ multithreading/20-tprintf.c
@@ -37,7 +37,6 @@ void mutex_destructor(void)
 int tprintf(char const *format, ...)
 {
 	va_list args;
-
 	pthread_t thread_num;
 
 	va_start(args, format);
@@ -47,13 +46,23 @@ int tprintf(char const *format, ...)
 		printf("format string error");
 		exit(EXIT_FAILURE);
 	}
+
+	if (pthread_mutex_lock(&mutex_print))
+	{
+		printf("mutex lock failed\n");
+		exit(EXIT_FAILURE);
+	}
 	thread_num = pthread_self();
 
 	printf("[%lu] ", thread_num);
 	vprintf(format, args);
 
+	if (pthread_mutex_unlock(&mutex_print))
+	{
+		printf("mutex unlock failed");
+		exit(EXIT_FAILURE);
+	}
 	va_end(args);
 
 	return (EXIT_SUCCESS);
 }
-
