ff37ba99b010d1816730094d72a5d8e13f3f2cf5
jorbian <5533@holbertonstudents.com>
2023-10-16 15:31:49 -0500

    Inital upload of some task files.

---
+++ sockets/1-server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sockets.h"
+
+/**
+ * main - the description of the program
+ * @argc: number of arguments that were fed to it
+ * @argv: value of the arguments
+ *
+ * Return: Always returns '0' --
+*/
+int main(int argc, char **argv)
+{
+	return (0);
+}
