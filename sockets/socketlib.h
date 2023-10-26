e0976d384c0153ab9cb2e81717e3738cafd2e868
jorbian <5533@holbertonstudents.com>
2023-10-25 19:11:44 -0500

    Fixed broken declarations

--- sockets/socketlib.h
+++ sockets/socketlib.h
@@ -12,5 +12,7 @@ typedef struct sockaddr_in saddr_t;
 
 int8_t initiate_socket(int16_t *fd, int16_t port);
 int8_t start_listening(int16_t *fd, int16_t port);
+void accept_connection(int16_t *server, int16_t *client);
+void request_received(int16_t *client);
 
 #endif
