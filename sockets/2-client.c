8b844c87191a9a0b36510adb9eb269173578a634
jorbian <5533@holbertonstudents.com>
2023-10-25 21:48:48 -0500

    typedefs should be avoided in .c filestypedefs should be avoided in .c filestypedefs should be avoided in .c files

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -11,19 +11,6 @@
 #include "socketlib.h"
 
 /**
- * struct connection_s - struct that represents a network connection
- * @sock_fd: file descriptor for the connection socket
- * @server_addr: nested struct for server_addr
- * @host_addr: asdf asdf
-*/
-typedef struct connection_s {
-    int sock_fd;
-	struct sockaddr_in server_addr;
-	struct hostent *host_addr;
-} connect_t;
-
-
-/**
  * throw_error - write error message to stderr and peace out
  * @error_type: a predefined error message
  * @detail: detail to fill into that message
--- sockets/socketlib.h
+++ sockets/socketlib.h
@@ -9,6 +9,19 @@
 #define DEFAULT_BUFF_SIZE 1024
 #define DEFAULT_MESSAGE "HTTP/1.1 200 OK\n"
 
+/**
+ * struct connection_s - struct that represents a network connection
+ * @sock_fd: file descriptor for the connection socket
+ * @server_addr: nested struct for server_addr
+ * @host_addr: asdf asdf
+*/
+typedef struct connection_s
+{
+	int sock_fd;
+	struct sockaddr_in server_addr;
+	struct hostent *host_addr;
+} connect_t;
+
 typedef struct sockaddr_in saddr_t;
 
 int spinup_server(void);
